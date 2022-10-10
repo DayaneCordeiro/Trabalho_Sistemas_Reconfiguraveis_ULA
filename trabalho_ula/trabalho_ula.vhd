b68cabaf708b50d16b5bb6cee4d009acd21c653c
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2022-10-10 16:55:14 -0300

    ajustes no código para a especificação

--- trabalho_ula/trabalho_ula.vhd
+++ trabalho_ula/trabalho_ula.vhd
@@ -4,10 +4,13 @@ USE ieee.std_logic_unsigned.all;
 ----------------------------------------
 ENTITY trabalho_ula IS
 	PORT (
-		a, b: IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-		op: IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-		r: OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-		z: OUT STD_LOGIC
+		input_a, input_b: IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+		operator: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		carry_in: IN STD_LOGIC
+		
+		result: OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+		carry_out: OUT STD_LOGIC
+		zero_out: OUT STD_LOGIC
 	);
 END ENTITY;
 ----------------------------------------
@@ -16,15 +19,16 @@ ARCHITECTURE trabalho_ula OF alu IS
 SIGNAL aux : STD_LOGIC_VECTOR(7 DOWNTO 0);
 
 BEGIN
-	WITH op SELECT
-		aux <= a AND b WHEN "000",
-			a OR b WHEN "001",
-			a XOR b WHEN "010",
-			NOT a WHEN "011",
-			a SLL WHEN "100",
-			a SRL WHEN "101",
-			a + b WHEN "110",
-			a - b WHEN "111";
-			r <= aux;
-			z <= '1' WHEN aux = "00000000" ELSE '0';
+	WITH operator SELECT
+		aux <= input_a AND input_b WHEN "000",
+			input_a OR input_b WHEN "001",
+			input_a XOR input_b WHEN "010",
+			NOT input_a WHEN "011",
+			input_a SLL WHEN "100",
+			input_a SRL WHEN "101",
+			input_a + input_b WHEN "110",
+			input_a - input_b WHEN "111";
+			
+			result <= aux;
+			zero_out <= '1' WHEN aux = "00000000" ELSE '0';
 END trabalho_ula;
